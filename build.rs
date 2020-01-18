3530d36b99253444889945024f8ef591dcdfed63
Marcin Kolny <mkolny@amazon.com>
2020-01-18 09:19:39 +0000

    allow to compile bindings with system-wide hawktracer (#5)

--- build.rs
+++ build.rs
@@ -2,8 +2,13 @@
 extern crate bindgen;
 extern crate cmake;
 
+#[cfg(feature = "pkg-config")]
+extern crate pkg_config;
+extern crate itertools;
+
 use std::env;
 use std::path::PathBuf;
+use itertools::Itertools;
 
 //In order to build 64 bit hawktracer:
 // mkdir build
@@ -11,34 +16,20 @@ use std::path::PathBuf;
 // cmake .. -G "Visual Studio 15 2017 Win64" -T v141,host=x64
 // cmake --build .
 fn main() {
-    build_project();
-    let mut extra_include_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    extra_include_path.push("build");
-    extra_include_path.push("lib");
-    extra_include_path.push("include");
+    let (main_header_path, header_paths) = if cfg!(feature = "pkg-config") {
+        pkg_config()
+    } else {
+        build_project();
+        let mut extra_include_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        extra_include_path.push("build");
+        extra_include_path.push("lib");
+        extra_include_path.push("include");
+        ("hawktracer/lib/include/hawktracer.h", vec![extra_include_path, PathBuf::from("./hawktracer/lib/include/")])
+    };
 
-    #[cfg(feature = "generate_bindings")]
-    {
-        let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        let bindings = bindgen::Builder::default()
-            .header("hawktracer/lib/include/hawktracer.h")
-            .clang_arg("-I./hawktracer/lib/include/")
-            .clang_arg(format!("-I{}", extra_include_path.display()))
-            .blacklist_type("max_align_t")
-            .generate()
-            .expect("Unable to generate bindings");
-        println!("Manifest dir: {:?}", manifest_dir);
+    println!("cargo:include={}", header_paths.iter().map(|p| p.display().to_string()).join(";"));
 
-        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-        bindings
-            .write_to_file(out_path.join("bindings.rs"))
-            .expect("Couldn't write bindings!");
-    }
-
-    #[cfg(not(feature = "generate_bindings"))]
-    {
-        copy_pregenerated_bindings();
-    }
+    generate_bindings(main_header_path, header_paths);
 
     let mut build_output_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     build_output_path.push("build");
@@ -74,7 +65,11 @@ fn main() {
         "cargo:rustc-link-search=all={}",
         build_output_path.display()
     );
-    println!("cargo:rustc-link-lib=static=hawktracer");
+    if cfg!(feature = "pkg-config") {
+        println!("cargo:rustc-link-lib=hawktracer");
+    } else {
+        println!("cargo:rustc-link-lib=static=hawktracer");
+    }
 }
 
 fn build_project() {
@@ -95,8 +90,29 @@ fn build_project() {
         .build();
 }
 
+#[cfg(feature = "generate_bindings")]
+fn generate_bindings(main_header_path: &str, header_paths: Vec<PathBuf>) {
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let bindings = bindgen::Builder::default()
+        .header(main_header_path)
+        .clang_args(
+            header_paths
+                .into_iter()
+                .map(|path| format!("-I{}", path.display())),
+        )
+        .blacklist_type("max_align_t")
+        .generate()
+        .expect("Unable to generate bindings");
+    println!("Manifest dir: {:?}", manifest_dir);
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
 #[cfg(not(feature = "generate_bindings"))]
-fn copy_pregenerated_bindings() {
+fn generate_bindings(_main_header_path: &str, _header_paths: Vec<PathBuf>) {
     use std::fs;
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let crate_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
@@ -105,4 +121,18 @@ fn copy_pregenerated_bindings() {
         out_path.join("bindings.rs"),
     )
     .expect("Couldn't find pregenerated bindings!");
+}
+
+#[cfg(feature = "pkg-config")]
+fn pkg_config() -> (&'static str, Vec<PathBuf>) {
+    let library = pkg_config::Config::new()
+        .cargo_metadata(!cfg!(feature = "non-cargo"))
+        .probe("hawktracer")
+        .expect("Can't probe for hawktracer in pkg-config");
+    ("wrapper.h", library.include_paths)
+}
+
+#[cfg(not(feature = "pkg-config"))]
+fn pkg_config() -> (&'static str, Vec<PathBuf>) {
+    unimplemented!()
 }
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include <hawktracer.h>
