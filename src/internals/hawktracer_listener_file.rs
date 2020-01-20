ea7d720e1f0e857380f7dcb336bcea308f1e20d9
Marcin Kolny <marcin.kolny@gmail.com>
2020-01-20 08:44:26 +0000

    Use simplified and more secure interfaces from hawktracer to create listeners

--- src/internals/hawktracer_listener_file.rs
+++ src/internals/hawktracer_listener_file.rs
@@ -4,7 +4,6 @@ use std::path::PathBuf;
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 pub struct HawktracerListenerFile {
-    listener: *mut _HT_FileDumpListener,
 }
 
 impl<'a> HawktracerListener<'a> for HawktracerListenerFile {}
@@ -13,32 +12,22 @@ impl HawktracerListenerFile {
     pub fn new(file_path: PathBuf, buffer_size: usize) -> HawktracerListenerFile {
         let string_path = file_path.into_os_string().into_string().unwrap();
         let file_path = std::ffi::CString::new(string_path).unwrap();
-        let listener = unsafe {
-            let listener = ht_file_dump_listener_create(
+        unsafe {
+            ht_file_dump_listener_register(
+                ht_global_timeline_get(),
                 file_path.as_ptr(),
                 buffer_size,
                 std::ptr::null_mut() as _,
             );
-
-            ht_timeline_register_listener(
-                ht_global_timeline_get(),
-                Some(ht_file_dump_listener_callback),
-                listener as _,
-            );
-
-            listener
         };
 
-        HawktracerListenerFile { listener: listener }
+        // TODO: For backward compatibility only. Remove on next API change
+        HawktracerListenerFile { }
     }
 }
 
+// TODO: For backward compatibility only. Remove on next API change
 impl Drop for HawktracerListenerFile {
     fn drop(&mut self) {
-        unsafe {
-            ht_timeline_flush(ht_global_timeline_get());
-            ht_timeline_unregister_all_listeners(ht_global_timeline_get());
-            ht_file_dump_listener_destroy(self.listener);
-        }
     }
 }
--- src/internals/hawktracer_listener_tcp.rs
+++ src/internals/hawktracer_listener_tcp.rs
@@ -4,38 +4,26 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 impl<'a> HawktracerListener<'a> for HawktracerListenerTCP {}
 
-pub struct HawktracerListenerTCP {
-    listener: *mut HT_TCPListener,
-}
+pub struct HawktracerListenerTCP {}
 
 impl HawktracerListenerTCP {
     pub fn new(port: u32, buffer_size: usize) -> HawktracerListenerTCP {
-        let listener = unsafe {
-            let listener = ht_tcp_listener_create(
+        unsafe {
+            ht_tcp_listener_register(
+                ht_global_timeline_get(),
                 port as i32,
                 buffer_size,
                 std::ptr::null_mut() as _,
             );
-
-            ht_timeline_register_listener(
-                ht_global_timeline_get(),
-                Some(ht_tcp_listener_callback),
-                listener as _,
-            );
-
-            listener
         };
 
-        HawktracerListenerTCP { listener: listener }
+        // TODO: For backward compatibility only. Remove on next API change
+        HawktracerListenerTCP { }
     }
 }
 
+// TODO: For backward compatibility only. Remove on next API change
 impl Drop for HawktracerListenerTCP {
     fn drop(&mut self) {
-        unsafe {
-            ht_timeline_flush(ht_global_timeline_get());
-            ht_timeline_unregister_all_listeners(ht_global_timeline_get());
-            ht_tcp_listener_destroy(self.listener);
-        }
     }
 }
